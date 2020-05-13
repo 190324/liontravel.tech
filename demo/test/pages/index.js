34025896dcf7c524cf382753287da1cce5058127
FM <fmkuo@liontravel.com>
2020-05-13 14:04:37 +0800

    getserversideprops

--- demo/test/pages/index.js
+++ demo/test/pages/index.js
@@ -9,26 +9,38 @@ const Page = ({ name }) => {
   );
 };
 
-export async function getStaticProps() {
-  const res = await fetch("https://www.omdbapi.com/?i=tt0076759");
-  const json = await res.json();
-  console.log("getStaticProps...", Date.now());
-  return {
-    props: {
-      name: json.Error,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const res = await fetch("https://www.omdbapi.com/?i=tt0076759");
+//   const json = await res.json();
+//   console.log("getStaticProps...", Date.now());
+//   return {
+//     props: {
+//       name: json.Error,
+//     },
+//   };
+// }
+
+// export async function getServerSideProps(context) {
+//   const res = await fetch("https://www.omdbapi.com/?i=tt0076759");
+//   const json = await res.json();
+//   console.log("getStaticProps...", Date.now());
+//   console.log("getStaticProps...", Date.now());
+//   return {
+//     props: {
+//       name: json.Error,
+//     }, // will be passed to the page component as props
+//   }
+// }
 
 /**
  * 首次載入會由 Server Render
  * 載入後的路由 Client Render
  */
-// Page.getInitialProps = async ctx => {
-//     const res = await fetch('https://www.omdbapi.com/?i=tt0076759')
-//     const json = await res.json()
-//     console.log("getInitialProps...", Date.now())
-//     return { name: json.Error }
-// }
+Page.getInitialProps = async ctx => {
+    const res = await fetch('https://www.omdbapi.com/?i=tt0076759')
+    const json = await res.json()
+    console.log("getInitialProps...", Date.now())
+    return { name: json.Error }
+}
 
 export default Page;
